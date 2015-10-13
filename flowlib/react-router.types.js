9b58cdf736046300a61c66b0b110f45680217d59
nheyn <nick@heyn.io>
2015-10-13 15:26:46 -0600

    Add Flow Static Type Checking
    Add files needed for Flowtype static type checking.

---
+++ flowlib/react-router.types.js
@@ -0,0 +1,15 @@
+declare module "react-router" {
+	declare var Route: ReactClass;
+	declare var RoutingContext: ReactClass;
+	declare var match: (settings: MatchFuncSettings, callback: MatchFuncCallback) => void;
+}
+
+type ReactRouterHandler<D,P,S> = ReactClass<D,P,S>;
+type ReactRouterRoute<D,P,S> = ReactElement<D,P,S>;
+
+type MatchFuncSettings = { routes: ReactRouterRoute<any, any, any>, location: string };
+type MatchFuncCallback = (
+	err: ?Error,
+	redirectLocation: {pathname: string, search: string},
+	renderProps: {[key: string]: any }
+) => void;
